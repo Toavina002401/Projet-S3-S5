f62f0f0362d4eb761231282c453f4d670fb42041
Belouh Andrianina <toavinawukeys@gmail.com>
2025-01-07 15:28:12 +0300

    Debut-Seance1

--- Pharmacie/sql/Conception.sql
+++ Pharmacie/sql/Conception.sql
@@ -1,3 +1,24 @@
+CREATE TABLE TypeAge(
+   id SERIAL,
+   libele VARCHAR(255)  NOT NULL,
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE Categorie(
+   id SERIAL,
+   specialiste VARCHAR(250)  NOT NULL,
+   PRIMARY KEY(id)
+);
+
+
+CREATE TABLE Corps(
+   id SERIAL,
+   partie VARCHAR(250)  NOT NULL,
+   id_categorie INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_categorie) REFERENCES Categorie(id) ON DELETE CASCADE
+);
+
 CREATE TABLE Laboratoires(
    id SERIAL,
    nom_laboratoire VARCHAR(255)  NOT NULL,
@@ -5,12 +26,16 @@ CREATE TABLE Laboratoires(
    PRIMARY KEY(id)
 );
 
+
 CREATE TABLE Maladies(
    id SERIAL,
    nom_maladie VARCHAR(255)  NOT NULL,
    symptomes VARCHAR(255) ,
-   PRIMARY KEY(id)
+   id_corps INTEGER NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_corps) REFERENCES Corps(id) ON DELETE CASCADE
 );
+
 
 CREATE TABLE Produits(
    id SERIAL,
@@ -39,3 +64,16 @@ CREATE TABLE Maladies_Produits(
    FOREIGN KEY(id_maladie) REFERENCES Maladies(id)  ON DELETE CASCADE,
    FOREIGN KEY(id_produit) REFERENCES Produits(id)  ON DELETE CASCADE
 );
+
+CREATE TABLE Produits_TypeAge(
+   id_produit INTEGER,
+   id_age INTEGER,
+   PRIMARY KEY(id_produit, id_age),
+   FOREIGN KEY(id_produit) REFERENCES Produits(id),
+   FOREIGN KEY(id_age) REFERENCES TypeAge(id)
+);
+
+
+
+
+
